49604c78f68ec5115341471b2bd8668fc79f981a
Julia Alves <julia.barroso@edu.unifil.br>
2026-08-12 17:05:01 -0300

    Add Livro class with title, author, and pages

---
+++ Exercícios/AT04/Livro.java
@@ -0,0 +1,12 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int paginas;
+
+    public Livro(String titulo, String autor, int paginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+}
